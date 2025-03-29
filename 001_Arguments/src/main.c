c857e33792012bb0c9b9ae8a8a5e45ab47757515
dagmtz <dagmtzs@gmail.com>
2025-03-29 17:03:00 -0600

    Initial commit, add two examples

---
+++ 001_Arguments/src/main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char** argv){
+  
+  printf("Hello, world! :)\n");
+  printf("This program was launched as: \033[1;37m%s\033[0m and was invoked with: \033[1;37m%d\033[0m extra arguments.\n", argv[0], argc-1);
+  
+  return 0;
+
+}
