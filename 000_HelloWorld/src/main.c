d5f91ae47fe7caedfc8defb73df192c24d63683a
dagmtz <dagmtzs@gmail.com>
2025-03-30 11:19:04 -0600

    Replace return values in main-s

--- 000_HelloWorld/src/main.c
+++ 000_HelloWorld/src/main.c
@@ -5,6 +5,6 @@ int main(int argc, char** argv){
   
   printf("Hello, world! :)\n");
 
-  return 0;
+  return EXIT_SUCCESS;
 
 }
--- 001_Arguments/src/main.c
+++ 001_Arguments/src/main.c
@@ -6,6 +6,6 @@ int main(int argc, char** argv){
   printf("Hello, world! :)\n");
   printf("This program was launched as: \033[1;37m%s\033[0m and was invoked with: \033[1;37m%d\033[0m extra arguments.\n", argv[0], argc-1);
   
-  return 0;
+  return EXIT_SUCCESS;
 
 }
