3887103d3ec6f59fe1292b333614a0f259febf63
dagmtz <dagmtzs@gmail.com>
2025-04-21 23:02:51 -0600

    format c source template

---
+++ 000_Test/main.c
@@ -0,0 +1,14 @@
+/*
+ *
+ *  File:             __NAME__
+ *  Author:           Daniel Martinez 
+ *                    dagmtzs@gmail.com
+ *  Creation:         Sat Apr 12 10:55:45 AM CST 2025
+ *  
+ *  Description:      
+ *
+ *  Modified:         __MODIFIED_ON__
+ *
+ */
+
+
---
+++ 002_NameSplitter/main.c
@@ -0,0 +1,18 @@
+
+/*
+ *
+ *  File:             main.c
+ *  Author:           Daniel Martinez 
+ *                    dagmtzs@gmail.com
+ *  Creation:         Mon Apr 21 10:55:15 PM CST 2025
+ *  
+ *  Description:      This program receives a name or a phrase (a string) and
+ *  splits it by spaces, so it outputs single words. 
+ *
+ *  Modified:         __MODIFIED_ON__
+ *
+ */
+
+
+
+
